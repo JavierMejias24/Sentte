db805b9f5dffa9d1fb8cede7c57309911fde8f31
JavierMejias24 <83413907+JavierMejias24@users.noreply.github.com>
2021-09-04 14:37:25 -0400

    index prueba

--- senttevaluation/senttevaluationapp/urls.py
+++ senttevaluation/senttevaluationapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('grupos', views.grupos, name="Grupos"),
     path('graficos', views.graficos, name="Graficos"),
     path('ayuda', views.ayuda, name="Ayuda"),
+    path('index', views.index, name="Index"),
 ]
