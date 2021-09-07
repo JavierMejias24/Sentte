c77214b70225b95708a1d6dd5b6121324a48ae2b
hans-burm <ha.burgos@alumnos.duoc.cl>
2021-09-07 17:17:27 -0300

    intento

--- senttevaluation/senttevaluationapp/urls.py
+++ senttevaluation/senttevaluationapp/urls.py
@@ -8,4 +8,13 @@ urlpatterns = [
     path('grupos', views.grupos, name="Grupos"),
     path('graficos', views.graficos, name="Graficos"),
     path('ayuda', views.ayuda, name="Ayuda"),
+    path('adminInicio', views.adminInicio, name="adminInicio"),
+    path('adminAcciones', views.adminAcciones, name="adminAcciones"),
+    path('adminCargos', views.adminCargos, name="adminCargos"),
+    path('adminCompetencias', views.adminCompetencias, name="adminCompetencias"),
+    path('adminGerencias', views.adminGerencias, name="adminGerencias"),
+    path('adminSubgerencias', views.adminSubgerencias, name="adminSubgerencias"),
+    path('adminUsuarios', views.adminUsuarios, name="adminUsuarios"),
+    path('adminEvaluaciones', views.adminEvaluaciones, name="adminEvaluaciones"),
+    path('adminAyuda', views.adminAyuda, name="adminAyuda"),
 ]
--- senttevaluation/senttevaluationapp/views.py
+++ senttevaluation/senttevaluationapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-
+# ----------------------------------  Evaluador ---------------------------------.
 def home(request):
     return render(request, "home.html")
 
@@ -16,3 +16,31 @@ def ayuda(request):
 
 def login(request):
     return render(request, "login.html")
+
+# ----------------------------------  Administrador ---------------------------------.
+def adminInicio(request):
+    return render(request, "adminInicio.html")
+
+def adminAcciones(request):
+    return render(request, "adminAcciones.html")
+
+def adminCargos(request):
+    return render(request, "adminCargos.html")
+
+def adminCompetencias(request):
+    return render(request, "adminCompetencias.html")
+
+def adminGerencias(request):
+    return render(request, "adminGerencias.html")
+
+def adminSubgerencias(request):
+    return render(request, "adminSubgerencias.html")
+
+def adminUsuarios(request):
+    return render(request, "adminUsuarios.html")
+
+def adminEvaluaciones(request):
+    return render(request, "adminEvaluaciones.html")
+
+def adminAyuda(request):
+    return render(request, "adminAyuda.html")
