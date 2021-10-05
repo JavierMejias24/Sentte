16bbefa40892bd5f9183ef62ec8803b913a65157
JaviMejias <mejiasjavier1997@gmail.com>
2021-10-05 12:51:29 -0500

    admin interface fix rol choices

--- senttevaluation/senttevaluationapp/admin.py
+++ senttevaluation/senttevaluationapp/admin.py
@@ -3,15 +3,71 @@ from .models import Area, Gerencia, SubGerencia, Empleado, PerfilRol, Cargo, Per
 
 # Register your models here.
 
+
+# ----------------------------------  Area ---------------------------------.
 admin.site.register(Area)
-admin.site.register(Gerencia)
-admin.site.register(SubGerencia)
-admin.site.register(Empleado)
-admin.site.register(PerfilRol)
+# ----------------------------------  Gerencia ---------------------------------.
+class GerenciaAdmin(admin.ModelAdmin):
+    list_display = ['NombreGerencia', 'IdArea']
+    list_editable = ['IdArea']
+
+admin.site.register(Gerencia, GerenciaAdmin)
+# ----------------------------------  SubGerencia ---------------------------------.
+class SubGerenciaAdmin(admin.ModelAdmin):
+    list_display = ['NombreSubgerencia', 'IdGerencia']
+    list_editable = ['IdGerencia']
+
+admin.site.register(SubGerencia, SubGerenciaAdmin)
+# ----------------------------------  Empleado ---------------------------------.
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ['Rut', 'Nombre', 'Contraseña', 'Correo', 'IdSubGerencia', 'IdRol']
+    list_editable = ['Nombre', 'Contraseña', 'Correo', 'IdSubGerencia', 'IdRol']
+
+admin.site.register(Empleado, EmpleadoAdmin)
+# ----------------------------------  Perfil Rol ---------------------------------.
+class PerfilRolAdmin(admin.ModelAdmin):
+    list_display = ['Rol', 'RelacionEvaluado', 'NombreEvaluador', 'NombreCalibrador']
+    list_editable = ['RelacionEvaluado', 'NombreEvaluador', 'NombreCalibrador']
+
+admin.site.register(PerfilRol, PerfilRolAdmin)
+# ----------------------------------  Cargo ---------------------------------.
+class PerfilRolAdmin(admin.ModelAdmin):
+    list_display = ['Rol', 'RelacionEvaluado', 'NombreEvaluador', 'NombreCalibrador']
+    list_editable = ['RelacionEvaluado', 'NombreEvaluador', 'NombreCalibrador']
+
 admin.site.register(Cargo)
-admin.site.register(Perfil)
-admin.site.register(Evaluacion)
-admin.site.register(PlanAccion)
-admin.site.register(DetalleEv)
-admin.site.register(Competencia)
+# ----------------------------------  Perfil ---------------------------------.
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ['NombrePerfil', 'IdCargo']
+    list_editable = ['IdCargo']
+
+admin.site.register(Perfil, PerfilAdmin)
+# ----------------------------------  Evaluacion ---------------------------------.
+class EvaluacionAdmin(admin.ModelAdmin):
+    list_display = ['Estado', 'Fase', 'ComentarioCalibrador', 'IdPerfil', 'IdEmpleado']
+    list_editable = ['Fase', 'IdPerfil', 'IdEmpleado']
+
+admin.site.register(Evaluacion, EvaluacionAdmin)
+# ----------------------------------  Plan Accion ---------------------------------.
+class PlanAccionAdmin(admin.ModelAdmin):
+    list_display = ['Accion', 'Medicion']
+
+admin.site.register(PlanAccion, PlanAccionAdmin)
+# ----------------------------------  Detalle Evaluacion ---------------------------------.
+class DetalleEvAdmin(admin.ModelAdmin):
+    list_display = ['FechaEvaluacion', 'ComentarioEvaluador', 'Calificacion', 'AutoEvaluacion' ,'IdEvaluacion', 'IdPlanAccion']
+    list_editable = ['IdEvaluacion', 'IdPlanAccion']
+
+admin.site.register(DetalleEv, DetalleEvAdmin)
+# ----------------------------------  Competencia ---------------------------------.
+class CompetenciasAdmin(admin.ModelAdmin):
+    list_display = ['NombreCompetencia', 'Definicion', 'IdPerfil']
+    list_editable = ['Definicion', 'IdPerfil']
+
+admin.site.register(Competencia, CompetenciasAdmin)
+# ----------------------------------  AccionClave ---------------------------------.
+class AccionClaveAdmin(admin.ModelAdmin):
+    list_display = ['Descripcion', 'IdCompetencia']
+    list_editable = ['IdCompetencia']
+
 admin.site.register(AccionClave)
