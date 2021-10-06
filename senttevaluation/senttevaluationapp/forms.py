26983a4be5ad13b85895b654690ed6acd37ff955
JaviMejias <mejiasjavier1997@gmail.com>
2021-10-06 17:08:51 -0500

    Expandir forms

--- senttevaluation/senttevaluationapp/forms.py
+++ senttevaluation/senttevaluationapp/forms.py
@@ -6,37 +6,182 @@ from django.forms.models import model_to_dict
 from .models import AccionClave, Competencia, Empleado, Cargo, Gerencia, PerfilRol, SubGerencia
 
 class EmpleadoForm(forms.ModelForm):
-
     class Meta:
         model = Empleado
-        fields = '__all__'
+        fields = ['Rut','Nombre','Contraseña','Correo','IdSubGerencia','IdRol']
+        labels = {
+            'Rut': 'Rut del empleado',
+            'Nombre': 'Nombre',
+            'Contraseña': 'Contraseña',
+            'Correo': 'Correo',
+            'IdSubGerencia': 'Subgerencia',
+            'IdRol': 'Rol',
+        }
+        widgets = {
+            'Rut': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese rut',
+                }
+            ),
+            'Nombre': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese nombre y apellidos',
+                }
+            ),
+            'Contraseña': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese contraseña',
+                }
+            ),
+            'Correo': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese correo',
+                }
+            ),
+            'IdSubGerencia': forms.Select(
+            ),
+            'IdRol': forms.Select(                
+                choices=(
+                    (1, "Evaluador"),
+                    (2, "Evaluado"),
+                    (3, "Calibrador"),
+                )
+            )
+        }
       
 class PerfilRolForm(forms.ModelForm):
     class Meta:
         model = PerfilRol
-        fields = '__all__'
+        fields = ['Rol','RelacionEvaluado','NombreEvaluador','NombreCalibrador']
+        labels = {
+            'Rol': 'Rol',
+            'RelacionEvaluado': 'Relacion con el evaluado',
+            'NombreEvaluador': 'Nombre del evaluador',
+            'NombreCalibrador': 'Nombredel calibrador',
+        }
+        widgets = {
+            'Rol': forms.Select(
+                choices = {
+                    (1, "Evaluador"),
+                    (2, "Evaluado"),
+                    (3, "Calibrador"),
+                }
+            ),
+            'RelacionEvaluado': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese la relación',
+                }
+            ),
+            'NombreEvaluador': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese el nombre del evaluador',
+                }
+            ),
+            'NombreCalibrador': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese el nombre del calibrador',
+                }
+            )            
+        }
 
 class CargoForm(forms.ModelForm):
     class Meta:
         model = Cargo
-        fields = '__all__'
+        fields = ['NombreCargo']
+        labels = {
+            'NombreCargo': 'Nombre Cargo',
+        }
+        widgets = {
+            'NombreCargo': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese nombre del cargo',
+                }
+            )
+        }
 
 class CompetenciaForm(forms.ModelForm):
     class Meta:
         model = Competencia
-        fields = '__all__'
+        fields = ['NombreCompetencia','Definicion','IdPerfil']
+        labels = {
+            'NombreCompetencia': 'Nombre competencia',
+            'Definicion': 'Definicion',
+            'IdPerfil': 'Perfil',
+        }
+        widgets = {
+            'NombreCompetencia': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese nombre de la competencia',
+                }
+            ),
+            'Definicion': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese definición',
+                }
+            ),
+            'IdPerfil': forms.Select()
+        }
 
 class AccionesForm(forms.ModelForm):
     class Meta:
-        model = AccionClave
-        fields = '__all__'
+        model = AccionClave        
+        fields = ['Descripcion','IdCompetencia']
+        labels = {
+            'Descripcion': 'Descripción',
+            'IdCompetencia': 'Competencia',
+        }
+        widgets = {
+            'Descripcion': forms.Textarea(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese descripción',
+                }
+            ),
+            'IdCompetencia': forms.Select()
+        }
 
 class GerenciaForm(forms.ModelForm):
     class Meta:
         model = Gerencia
-        fields = '__all__'
+        fields = ['NombreGerencia','IdArea']
+        labels = {
+            'NombreGerencia': 'Nombre gerencia',
+            'IdArea': 'Area',
+        }
+        widgets = {
+            'NombreGerencia': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese nombre de la gerencia',
+                }
+            ),
+            'IdArea': forms.Select()
+        }
 
 class SubgerenciaForm(forms.ModelForm):
     class Meta:
         model = SubGerencia
-        fields = '__all__'
+        fields = ['NombreSubgerencia','IdGerencia']
+        labels = {
+            'NombreSubgerencia': 'Nombre subgerencia',
+            'IdGerencia': 'Gerencia',
+        }
+        widgets = {
+            'NombreSubgerencia': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese nombre de la subgerencia',
+                }
+            ),
+            'IdGerencia': forms.Select()
+        }
